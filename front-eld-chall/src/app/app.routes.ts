f6432c54b99c8e5dfa75b0095ddf89bdf9e84617
cteves <cristianteves1@gmail.com>
2024-09-23 08:45:45 -0300

    feat: estructura de carpetas para la escalabilidad de la aplicacion

--- front-eld-chall/src/app/app.routes.ts
+++ front-eld-chall/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './core/login/login.component';
 
-export const routes: Routes = [
-    { path: 'path', component: LoginComponent },
-    { path: '**', component: LoginComponent },
+const routes: Routes = [
+    { path: '', component: LoginComponent },
 ];
+  
